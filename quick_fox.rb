11139ad8450de3f782829d78c4ff42e5e4b05625
Alex Melcon <amelc001@fiu.edu>
2016-02-28 15:33:14 -0500

    initial commit

---
+++ quick_fox.rb
@@ -0,0 +1,14 @@
+class Fox
+  attr_accessor :jumped_over1
+  attr_accessor :jumped_over2
+  def intitialize jumped_over1,jumped_over2
+    @jumped_over1 = jumped_over1
+    @jumped_over2 - jumped_over2
+  end
+end
+
+
+quick_fox = Fox.new
+
+
+#quick_fox.jumped_over(lazy_dog, daisy_log)
